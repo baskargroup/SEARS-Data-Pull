ed64da773b28d8e2f5db39634f389870682fdc4e
Ronak Tali <rtali@iastate.edu>
2024-06-28 18:18:52 -0500

    Final Commit

--- ReadMe.md
+++ ReadMe.md
@@ -1,9 +1,9 @@
-### How to Query MongoDB using Python
+### How to Query MongoDB using Python to Download Data and Files
 
 #Steps
 
 0. Copy the `.env` file to the root directory of the project.
 1. Install all requirements using `pip3 install -r requirements.txt`
-2. Specify your query in `query.py`
-3. Specify the projection in `projection.py`
-4. Run `python3 main.py` to see the results.
+2. Run `python3 mongo_connect.py` to download data from MongoDB to a CSV file. Set `search_criteria` and `output_file_name` in the program file.
+3. Run `python3 DO_Download.py` to download files from Digital Ocean to a local directory `./file_fetch/`. All files related to experiments meeting the search criteria will be downloaded.
+4. Run your ML model on the downloaded data and files.
--- mongo_connect.py
+++ mongo_connect.py
@@ -1,36 +1,93 @@
 from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
 import os
+import pandas as pd
 
 
-def connect_to_mongo(db_name):
+def process_solvents(solvents):
+    CB = 0.0
+    DCB = 0.0
+    TOL = 0.0
+    for solvent in solvents:
+        if solvent['name'] == 'CB':
+            CB = solvent['value']
+        elif solvent['name'] == 'DCB':
+            DCB = solvent['value']
+        elif solvent['name'] == 'Tol':
+            TOL = solvent['value']
+
+    return [CB, DCB, TOL]
+
+
+def process_conductivity(conductivity):
+    CON = 0.0
+    num_rows = len(conductivity)
+    if num_rows > 0:
+        CON = conductivity[num_rows-1]['reading']
+    return CON
+
+
+def process_thickness(thickness):
+    THK = 0.0
+    num_rows = len(thickness)
+    if num_rows > 0:
+        THK = thickness[num_rows-1]['reading']
+    return THK
+
+
+def connect_to_mongo(db_name, search_criteria, output_file_name):
     # Load .env file
     load_dotenv(find_dotenv())
 
     # MONGODB
     CONN_STRING = os.environ.get('MONGO_URI')
-    print(CONN_STRING)
-    MONGODB_HOST = 'rtali:vqPi8ORyWQJGdJbs@isu.wet0vxf.mongodb.net'
+    # print(CONN_STRING)
+    MONGODB_HOST = 'mongodb+srv://rtali:vqPi8ORyWQJGdJbs@isu.wet0vxf.mongodb.net/?appName=ISU'
     MONGODB_PORT = 27017
     DB_NAME = 'dope'
     COLLECTION_NAME = 'productData'
 
+    # Create a DataFrame
+    df = pd.DataFrame(columns=['exp_id', 'experiment_name', 'CB', 'DCB',
+                      'TOL', 'annealing_temperature', 'conductivity', 'thickness'])
+
     # Connect to MongoDB
     try:
-
-        # client = MongoClient(CONN_STRING)
-        # client = MongoClient(MONGODB_HOST,serverSelectionTimeoutMS=10000)
-
-        client = MongoClient(CONN_STRING)
+        client = MongoClient(MONGODB_HOST)
         db = client[db_name]
         print('Connected to MongoDB')
         collection = db[COLLECTION_NAME]
+
         for doc in collection.find():
-            print(doc.get('experiment_name'))
+            if search_criteria in doc.get('experiment_name'):
+                row = []
+                row.append(doc.get('_id'))
+                row.append(doc.get('experiment_name'))
+                row.extend(process_solvents(doc.get('solvents')))
+                row.append(doc.get('annealing_temperature'))
+                row.append(process_conductivity(doc.get('conductivity')))
+                row.append(process_thickness(doc.get('thickness')))
+
+                # Append to DataFrame and index is false
+                df.loc[len(df)] = row
+
+        # Create a text file with just the experiment ids. These are then used to download the files from Digital Ocean for each experiment
+        with open('./file_fetch/payload.txt', 'w') as file:
+            for exp_id in df['exp_id']:
+                file.write(str(exp_id) + '\n')
+
+        # Save DataFrame to CSV
+        df.to_csv(output_file_name, index=False)
+
     except Exception as e:
-        print('Error connecting to MongoDB:', e)
+        print(e)
 
 
 if __name__ == '__main__':
+
     db_name = 'dope'
-    connect_to_mongo(db_name=db_name)
+    search_criteria = "Spring24_SolvTemp_JPM"
+    output_file_name = "./file_fetch/spring24_solvtemp_jpm.csv"
+
+    connect_to_mongo(db_name=db_name, search_criteria=search_criteria,
+                     output_file_name=output_file_name)
